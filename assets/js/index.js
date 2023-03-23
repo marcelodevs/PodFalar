2e37b414a514746d08aa6948e4a0cffa706f0118
LuckMG <128225332+marcelodevs@users.noreply.github.com>
2023-03-23 12:17:26 -0300

    Versão 3
    Inicialização do carrossel

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,3 @@
-/* TEXTO APARECENDO */
-
-function typeWrite(elemento) {
-    // O método .split() é usado para fragmentar o texto e aplicar as propriedades
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    // A cada instante, o forEach vai "atualizar" e vai mostrar uma nova palavra
-    textoArray.forEach((letra, i) => {
-        /* O método setTimeout é um temporizador, o innerHTML vai ser responsável pelo
-        aparecimento das letras*/
-        setTimeout(() => elemento.innerHTML += letra, 50 * i)
-    });
-}
-
-// Seleciona a tag do texto principal do site
-const texto = document.querySelector('.podfalar-txt-main');
-typeWrite(texto); //Chama a função
-
 /* SCROLL DE VOLTAR PARA CIMA */
 
 function voltar() {
@@ -55,20 +37,5 @@ function reveal() {
 
 // Adiciona o evento "scroll" e chama a função "reveal"
 window.addEventListener("scroll", reveal);
-
-/* MENU */
-
-var menu = document.getElementById('menu');
-var threshold = 50;
-
-window.addEventListener('scroll', function () {
-    if (window.pageYOffset > threshold)
-    {
-        menu.classList.add('scrolled');
-    } else
-    {
-        menu.classList.remove('scrolled');
-    }
-});
 
 /* NAVBAR RESPONSIVO */
